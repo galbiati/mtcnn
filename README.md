2a5dc1292119eb043fdb3aa92ffddb271a0dc66a
gianni <galbiatig@gmail.com>
2020-01-10 20:27:30 -0500

    setup utilities

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ MTCNN end-to-end in PyTorch.
 
 Supports batched inputs by adding a column to the bounding box matrix for batch index.
 
-Keeps everything on CUDA device until done, if you like.
+If you use CUDA, everything stays on device through end of inference.
+
+Python 3 only; 3.7+ recommended.
 
 ## Credit
 Adapted from [Dan Antoshchenko's implementation](https://github.com/TropComplique/mtcnn-pytorch)
--- setup.py
+++ setup.py
@@ -4,7 +4,21 @@
 # By: Gianni Galbiati
 
 # Standard libraries
+from setuptools import find_packages, setup
 
 # External libraries
 
 # Internal libraries
+
+
+setup(
+    name='mtcnn',
+    version='0.1.0',
+    author='Gianni Galbiati',
+    author_email='galbiatig@gmail.com',
+    packages=find_packages(),
+    license='LICENSE.txt',
+    description='Pure PyTorch MTCNN',
+    long_description=open('README.md').read(),
+    include_package_data=True,
+)
